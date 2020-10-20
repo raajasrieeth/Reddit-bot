7b96f42ded643d749cc40e1ee341325a86ae100c
Raaj Asrieeth MR <asrieethraaj@gmail.com>
2020-10-20 10:38:22 +0530

    first update

---
+++ getstuff.py
@@ -0,0 +1,40 @@
+import praw  # to be pip installed
+
+class Getter():
+    reddit = praw.Reddit(   # initialize the reddit variable
+        client_id='UctJAJCE-8-nvA',
+        client_secret='1FsIcCXLLrKqxcAU3bRISfzuUrI',
+        user_agent = 'my user agent'
+
+    )
+    def __init__(self, subreddit , number):
+        self.sb = subreddit
+        self.n = number
+
+    def content(self):
+        '''Get the content . ie , the posts in the subreddit,
+         followed by the link contained in them , if any. '''
+        for submission in self.reddit.subreddit(self.sb).hot(limit=self.n):
+            print(submission.title)
+            print('=========================')
+            print(submission.selftext)
+            print('*************************')
+            print("URL to the full article: " , submission.url)
+
+    def comments(self):
+        '''Get the top comments in the subreddit'''
+        for comment in self.reddit.subreddit(self.sb).comments(limit=5):
+            print("++++++++++++++++++++++")
+            print(comment.body) # get the body of the comment
+            print("######################")
+
+    def main(self):
+        '''get the content '''
+        print(self.content())
+    def doc(self):
+        '''Used to get the docstring for a function.'''
+        for sub in self.reddit.subreddit(self.sb).hot(limit = 1):
+            print(sub.__doc__)
+if __name__ == '__main__':
+    get = Getter('news', 5)  # get 5 news bits
+    get.main()
---
+++ postnews.py
@@ -0,0 +1,8 @@
+import webbrowser
+import getstuff
+import requests
+# webbrowser.open('http://thisisraaj.unaux.com/wp-admin/post.php?post=48&action=edit')  # news page in my website.
+
+if __name__ == '__main__':
+    items = getstuff.Getter('news', 5)  # get 5 news bits daily!
+    items.content()
